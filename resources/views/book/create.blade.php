581c68b8f8819c7d7e763e4ddb5f738152d974f6
LansDoank <maulana09180@gmail.com>
2026-04-11 18:56:55 +0700

    laptop

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -53,8 +53,8 @@
                         </select>
                     </div>
                     <div>
-                        <label for="rack" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tingkat
-                            Kelas</label>
+                        <label for="rack" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Pilih Rak</label>
                         <select id="rack" name="rack"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             required>
